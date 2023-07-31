4cedd5a7e562b6b73d3fe09c6211a0643cb2bf36
Aresso93 <ares.fiumicelli@gmail.com>
2023-07-31 18:21:52 +0200

    grazie, ChatGPT

--- app.js
+++ app.js
@@ -3,45 +3,99 @@ const fs = require("fs");
 const inputUrl = process.argv[2];
 
 const splittedInputUrl = inputUrl.split('.');
-const output = '.'+splittedInputUrl[1] + '-report.' + splittedInputUrl[splittedInputUrl.length - 1];
+const output = '.' + splittedInputUrl[1] + '-report.' + splittedInputUrl[splittedInputUrl.length - 1];
 
+let data = readFile(inputUrl);
+if (data) {
+  const charactersCount = countCharacters(data);
+  const wordsCount = countWords(data);
+  const mostCommonCharacter = findMostCommonCharacter(data);
+  const mostCommonWord = findMostCommonWord(data);
 
+  const report = `Il file di input ha ${charactersCount} caratteri esclusi gli spazi, e ${wordsCount} parole.\nIl carattere più comune è '${mostCommonCharacter.char}' e appare ${mostCommonCharacter.count} volte.\nLa parola più comune è '${mostCommonWord.word}' e appare ${mostCommonWord.count} volte.`;
 
-let data = readFile(inputUrl)
- console.log(data)
-
-
- if (data) {
-
-    const result = 'ciao';
-
-    writeData(output, result);
+  console.log(report);
+  writeData(output, report);
 }
 
-function readFile(url){
-    try {
-        const data = fs.readFileSync(url, "utf8");
-        return data;
-    } catch (err) {
-        console.error(err.message);
-        return null;
+function readFile(url) {
+  try {
+    const data = fs.readFileSync(url, "utf8");
+    return data;
+  } catch (err) {
+    console.error(err.message);
+    return null;
+  }
+}
+
+function writeData(url, data) {
+  try {
+    fs.writeFileSync(url, data);
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+function countCharacters(text) {
+  // Remove spaces from the text before counting characters
+  const textWithoutSpaces = text.replace(/\s+/g, '');
+  return textWithoutSpaces.length;
+}
+
+function countWords(text) {
+  // Remove leading and trailing spaces from the text
+  const trimmedText = text.trim();
+  // Split the text into words using one or more spaces as the separator
+  const wordsArray = trimmedText.split(/\s+/);
+  return wordsArray.length;
+}
+function findMostCommonCharacter(text) {
+    const characterOccurrences = {};
+  
+    // Remove spaces from the text before counting characters
+    const textWithoutSpaces = text.replace(/\s+/g, '');
+  
+    for (const char of textWithoutSpaces) {
+      if (char !== ' ') {
+        characterOccurrences[char] = (characterOccurrences[char] || 0) + 1;
+      }
     }
-}
-
-
-function writeData(url, data){
-
-
-    try {
-        fs.writeFileSync(url, data);
-    } catch (err) {
-        console.error(err.message);
+  
+    let mostCommonChar = { char: '', count: -1 };
+    for (const char in characterOccurrences) {
+      if (characterOccurrences[char] > mostCommonChar.count) {
+        mostCommonChar.char = char;
+        mostCommonChar.count = characterOccurrences[char];
+      }
     }
-}
+  
+  
+    return mostCommonChar;
+  }
+  
+  
 
 
-function myFunction(){
+function findMostCommonWord(text) {
+  const wordMap = new Map();
 
+  // Remove leading and trailing spaces from the text
+  const trimmedText = text.trim();
+  // Split the text into words using one or more spaces as the separator
+  const wordsArray = trimmedText.split(/\s+/);
+
+  for (const word of wordsArray) {
+    wordMap.set(word, (wordMap.get(word) || 0) + 1);
+  }
+
+  let mostCommonWord = { word: '', count: 0 };
+  for (const [word, count] of wordMap.entries()) {
+    if (count > mostCommonWord.count) {
+      mostCommonWord = { word, count };
+    }
+  }
+
+  return mostCommonWord;
 }
 
 
